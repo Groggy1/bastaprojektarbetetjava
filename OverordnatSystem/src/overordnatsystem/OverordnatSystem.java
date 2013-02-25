14e70574003e072c59056176b020d85d1406da94
Groggy1 <groggy12@gmail.com>
2013-02-25 09:25:16 +0100

    Gjorde om NetBeansprojektet, något funkade inte med det föregående

---
+++ OverordnatSystem/src/overordnatsystem/OverordnatSystem.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package overordnatsystem;
+
+/**
+ *
+ * @author oskst764
+ */
+public class OverordnatSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
