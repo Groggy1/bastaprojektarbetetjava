d3892248e24a274ee4aad3b211bff173d7a2f86b
unknown <oskst764@ITN-TP4028-05.ad.liu.se>
2013-03-04 11:59:35 +0100

    Fixade for-loopar

--- OverordnatSystem/src/overordnatsystem/OptPlan.java
+++ OverordnatSystem/src/overordnatsystem/OptPlan.java
@@ -7,7 +7,6 @@ package overordnatsystem;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
-import java.lang.Math;
 
 /**
  *
@@ -26,8 +25,8 @@ public class OptPlan {
     public void createPlan() {
         nodes = new ArrayList<Vertex>();
         edges = new ArrayList<Edge>();
-        LinkedList<Vertex> path;
-        //ArrayList<LinkedList<Vertex>> paths;
+        LinkedList<Vertex> path = null;
+        //ArrayList<LinkedList<Vertex>> paths = null;
         int diff;
 
         for (int i = 0; i < ds.nodes; i++) {
@@ -37,7 +36,7 @@ public class OptPlan {
         }
 
         for (int i = 0; i < ds.arcs; i++) {
-            diff = (int) Math.max(Math.abs(ds.nodeY[ds.arcStart[i] - 1] - ds.nodeY[ds.arcEnd[i] - 1]),Math.abs(ds.nodeX[ds.arcStart[i] - 1] - ds.nodeX[ds.arcEnd[i] - 1]));
+            diff = (int) Math.max(Math.abs(ds.nodeY[ds.arcStart[i] - 1] - ds.nodeY[ds.arcEnd[i] - 1]), Math.abs(ds.nodeX[ds.arcStart[i] - 1] - ds.nodeX[ds.arcEnd[i] - 1]));
             Edge lane = new Edge("" + (i + 1), nodes.get(ds.arcStart[i] - 1), nodes.get(ds.arcEnd[i] - 1), diff);
             edges.add(lane);
         }
@@ -45,18 +44,21 @@ public class OptPlan {
         Graph graph = new Graph(nodes, edges);
         DijkstraAlgorithm dijkstra = new DijkstraAlgorithm(graph);
         // Compute shortest path
-        /*for (int i = 1; i <= ds.nodes; i++) {
+        for (int i = 1; i < ds.nodes; i++) {
             System.out.println("i" + i);
-            for (int j = 1; j <= ds.nodes; j++) {
-                System.out.println("j" + j);*/
-                dijkstra.execute(nodes.get(3));
-                path = dijkstra.getPath(nodes.get(32));
-                //paths.add(path);
-            //}
-        //}
+            for (int j = 1; j < ds.nodes; j++) {
+                System.out.println("j" + j);
+                if (i != j) {
+                    dijkstra.execute(nodes.get(i));
+                    path = dijkstra.getPath(nodes.get(j));
+                    //paths.add(path);
+                }
+            }
+        }
         // Get shortest path
         for (int i = 0; i < path.size(); i++) {
             System.out.println(path.get(i));
+            //System.out.println(paths.get(i));
         }
     }
 }
